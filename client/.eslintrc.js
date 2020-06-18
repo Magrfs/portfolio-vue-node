82176eff35ba9ca02275a29f662796ca0e0218c1
Magrfs <magnolirfsit@gmail.com>
2020-06-18 17:29:45 +0900

    complete Ch2

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,14 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
+    indent: ['error', 2],
+    'vue/html-indent': ['error', 2],
+    'vue/script-indent': ['error', 2],
+    'linebreak-style': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
