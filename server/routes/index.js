ed00c79f0e66441cc3709394c3bc1a0b4fb2460d
Magrfs <magnolirfsit@gmail.com>
2020-06-18 18:56:57 +0900

    complete ch3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const path = require("path"); // 추가
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("*", (req, res) => {
+    // router.get('/', (req, res) => { 변경
+    res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
+    // res.render('index', { title: 'Express' }); 변경
 });
 
 module.exports = router;
